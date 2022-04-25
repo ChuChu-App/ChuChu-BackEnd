1d325fbea590f8692f73a9edde73ca6bfb14eee8
gwemin <dbwnghks921@naver.com>
2022-04-25 19:17:04 +0900

    ✨BackEnd Init✨

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.get('/welcome', (req: Request, res: Response, next: NextFunction) => {
+  res.send('welcome!');
+});
+
+
+app.listen('3000', () => {
+  console.log(`
+  ################################################
+  🎉 Server listening on port: 3000 
+  🎉 http://localhost:3000
+  ################################################
+`);
+});
